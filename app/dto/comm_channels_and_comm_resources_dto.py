3d1c4b857e259c63c038cd33557b20156d87900f
waldyrturquetti <waldyrturquetti@bemol.com.br>
2024-01-06 18:27:26 +0000

    feat: done serialize

--- app/dto/comm_channels_and_comm_resources_dto.py
+++ app/dto/comm_channels_and_comm_resources_dto.py
@@ -7,12 +7,19 @@ class CommChannelsAndCommResourcesDto(graphene.ObjectType):
 
     channel = graphene.String()
     resource = graphene.String()
+    resource_type = graphene.String()
     weight = graphene.Float()
 
 
 def serialize_comm_channels_and_comm_resources(results):
-    print(results[0]['user'])
     comm_channels_and_comm_resources_dto_array = []
-
+    for result in results:
+        obj = {
+            'channel': result['communication_channel_type']['value'],
+            'resource': result['communication_resource_value']['value'],
+            'resource_type': result['communication_resource_type']['value'],
+            'weight': result['preference_weight']['value'],
+        }
+        comm_channels_and_comm_resources_dto_array.append(obj)
 
     return comm_channels_and_comm_resources_dto_array
